ec596969b04c779943ccb5ca59b762fdb945982f
m.basere <m.basere@baman.club>
2023-02-18 02:15:06 +0330

    implement Nucleotides Count

--- SangerClient/Handlers/Handler.cs
+++ SangerClient/Handlers/Handler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Framework.Commons;
 using Framework.Parser.FASTQs;
 using Framework.Utilities;
@@ -12,7 +13,22 @@ namespace SangerClient.Handlers
     {
         public static void Execute(bool flag, string filePath)
         {
-            GetFASTQSequenceCount(filePath);
+            try
+            {
+                if (flag)
+                {
+                    GetFASTQSequenceCount(filePath);
+                }
+                else
+                {
+                    GetFASTQNucleotidesCount(filePath);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(exception.Message);
+            }
         }
 
         private static void GetFASTQSequenceCount(string filePath)
@@ -32,6 +48,29 @@ namespace SangerClient.Handlers
             }
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Sequence Count: {list.Count}");
+        }
+
+        private static void GetFASTQNucleotidesCount(string filePath)
+        {
+            var fileInfo = new FileInfo(filePath);
+            IParser<FASTQSequence> parser = new FASTQParser();
+            var list = new List<FASTQSequence>();
+            using var fs = File.OpenRead(filePath);
+            long count;
+
+            if (fileInfo.Extension.ToLower() == ".gz")
+            {
+                using var gz = Helper.GZipReader(fs);
+                list.AddRange(parser.Parse(gz));
+                count = list.Sum(item => Encoding.UTF8.GetString(item.SequenceData).Length);
+            }
+            else
+            {
+                list = parser.Parse(fs).ToList();
+                count = list.Sum(item => Encoding.UTF8.GetString(item.SequenceData).Length);
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Nucleotides Count: {count}");
         }
     }
 }
