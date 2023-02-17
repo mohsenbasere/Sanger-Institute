d2bbfc98dbbe9ca6f83a19f0769c0ca0c48181a5
m.basere <m.basere@baman.club>
2023-02-18 02:02:31 +0330

    implement sequence count

--- SangerClient/Handlers/Handler.cs
+++ SangerClient/Handlers/Handler.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Framework.Commons;
+using Framework.Parser.FASTQs;
 
 namespace SangerClient.Handlers
 {
     public static class Handler
     {
-        public static void Execute(bool arg1, string arg2)
+        public static void Execute(bool flag, string filePath)
         {
-            Console.WriteLine("This is Test");
+            GetFASTQSequenceCount(filePath);
+        }
+
+        private static void GetFASTQSequenceCount(string filePath)
+        {
+            var list = new List<FASTQSequence>();
+            IParser<FASTQSequence> parser = new FASTQParser();
+            using var fs = File.OpenRead(filePath);
+            list = parser.Parse(fs).ToList();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Sequence Count: {list.Count}");
         }
     }
 }
