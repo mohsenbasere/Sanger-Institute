f1dc94ceb77d81719802dbae1085eaec2653ce9b
m.basere <m.basere@baman.club>
2023-02-18 01:50:28 +0330

    add command to client

---
+++ SangerClient/Handlers/Handler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SangerClient.Handlers
+{
+    public static class Handler
+    {
+        public static void Execute(bool arg1, string arg2)
+        {
+            Console.WriteLine("This is Test");
+        }
+    }
+}
--- SangerClient/Program.cs
+++ SangerClient/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Diagnostics;
+using Figgle;
+using SangerClient.Handlers;
 
-namespace ConsoleApp1
+namespace SangerClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(FiggleFonts.Standard.Render("Sanger Institute Test"));
+            if (Debugger.IsAttached)
+            {
+                args = new[] { "--file-path",
+                    @"1234.fastq",
+                    "--flag", "true" };
+                ConfigCommandSystem(args);
+                return;
+            }
+            ConfigCommandSystem(args);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void ConfigCommandSystem(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Option<string>(
+                    "--file-path",
+                    "Specify FASTQ file path."
+                ),
+                new Option<bool>(
+                    "--flag",
+                    "Specify the flag that means true ===> Sequence Count or  false ===> Nucleotides Count"),
+            };
+            rootCommand.Description =
+                "Client to execute commands for given file path and flag that show information depends on flag";
+            rootCommand.Handler = CommandHandler.Create<bool, string>(Handler.Execute);
+            rootCommand.Invoke(args);
         }
     }
 }
