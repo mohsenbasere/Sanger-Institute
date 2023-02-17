ef2875652264c4584608fe22efe49e7439635383
m.basere <m.basere@baman.club>
2023-02-18 02:05:42 +0330

    add helper for GZip Reader

---
+++ Framework/Utilities/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Framework.Utilities
+{
+    public static class Helper
+    {
+        public static GZipStream GZipReader(Stream stream)
+        {
+            return new GZipStream(stream, CompressionMode.Decompress);
+        }
+    }
+}
